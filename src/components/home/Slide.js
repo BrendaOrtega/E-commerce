133c24cc49b42a4c6ce17af8023cda2d61567225
Brenda Gonzalez <bremin11.20.93@gmail.com>
2018-03-16 15:20:23 -0600

    slide

--- src/components/home/Slide.js
+++ src/components/home/Slide.js
@@ -14,7 +14,7 @@ class Slide extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="slide">
                 <div>
                     <Carousel autoplay>
                         <div>
